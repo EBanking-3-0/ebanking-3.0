f89eff5b168508945314f7d602781a5dd6347be5
Khabir <hakim1khabir2@gmail.com>
2026-01-06 12:21:58 +0100

    fix(web): environment.kubernetes.ts

--- apps/frontend/web-app/src/environments/environment.kubernetes.ts
+++ apps/frontend/web-app/src/environments/environment.kubernetes.ts
@@ -1,7 +1,9 @@
+const apiUrl = (window as any).env?.apiUrl || 'https://bank-api.h4k5.net/graphql';
+
 export const environment = {
   production: true,
-  apiUrl: (window as any).env?.apiUrl || 'https://bank-api.h4k5.net/graphql',
-  apiRestUrl: (window as any).env?.apiRestUrl || 'https://bank-api.h4k5.net', // Gateway proxies this
+  apiUrl: apiUrl,
+  apiRestUrl: (window as any).env?.apiRestUrl || apiUrl.replace(/\/graphql$/, ''),
   keycloak: {
     url: (window as any).env?.keycloakUrl || 'https://bank-auth.h4k5.net',
     realm: (window as any).env?.keycloakRealm || 'ebanking-realm',
