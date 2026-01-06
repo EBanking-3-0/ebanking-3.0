bff76566f6ba30777b5239762d9c1fb184021df7
Adamo08 <omarelaaddam519@gmail.com>
2026-01-06 12:45:04 +0100

    refactor(SecurityConfig): remove unused imports for cleaner code

--- libs/shared/security/src/main/java/com/ebanking/security/SecurityConfig.java
+++ libs/shared/security/src/main/java/com/ebanking/security/SecurityConfig.java
@@ -4,15 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod; // Add this import
-// Add this import
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
