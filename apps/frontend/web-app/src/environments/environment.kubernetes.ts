904a035fbe7a8825d846b21a8b95d3420ec30282
Khabir <hakim1khabir2@gmail.com>
2026-01-06 02:14:05 +0100

    chors(user-service): replace localhost with environments vars

--- apps/frontend/web-app/src/environments/environment.kubernetes.ts
+++ apps/frontend/web-app/src/environments/environment.kubernetes.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: (window as any).env?.apiUrl || 'https://bank-api.h4k5.net/graphql',
+  kycApiUrl: (window as any).env?.kycApiUrl || 'https://bank-api.h4k5.net/api/v1/kyc',
   keycloak: {
     url: (window as any).env?.keycloakUrl || 'https://bank-auth.h4k5.net',
     realm: (window as any).env?.keycloakRealm || 'ebanking-realm',
